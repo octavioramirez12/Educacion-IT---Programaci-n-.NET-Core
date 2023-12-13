f9c136688671603bb75572d7cc8b2f971e41ba5f
Octavio Ramírez <octavio.ramirez.3980@gmail.com>
2023-12-12 23:18:20 -0300

    Agregar archivos de proyecto.

---
+++ Educacion IT - Programación .NET/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Educacion_IT___Programación_.NET.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Educacion_IT___Programación_.NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Educacion IT - Programación .NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Educacion_IT___Programación_.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
