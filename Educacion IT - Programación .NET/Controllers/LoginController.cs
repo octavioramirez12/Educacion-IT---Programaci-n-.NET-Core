7563512563c0c8a1f5ded1a9068e9ac458518395
Octavio Ramírez <octavio.ramirez.3980@gmail.com>
2023-12-13 21:14:55 -0300

    Clase numero 3

---
+++ Educacion IT - Programación .NET/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Educacion_IT___Programación_.NET.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: LoginController
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
