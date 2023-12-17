7ff43ec03e98626b02344dcc0fd6e4dab5f4031c
Octavio Ramírez <octavio.ramirez.3980@gmail.com>
2023-12-17 20:44:12 -0300

    Se agrega referencia a program de la api al connection string del launchsettings

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,11 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
+using Data;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+ApplicationDbContext.ConnectionString = builder.Configuration.GetConnectionString("ApplicationDbContext");
 
 var app = builder.Build();
 
