b4cbc5ceec79e03bd592e8fbf16dd6b454429981
Octavio Ramírez <octavio.ramirez.3980@gmail.com>
2023-12-17 22:09:00 -0300

    Se agrega consumo de datos en el datatable desde la api

--- Educacion IT - Programación .NET/wwwroot/js/Usuarios/Usuarios.js
+++ Educacion IT - Programación .NET/wwwroot/js/Usuarios/Usuarios.js
@@ -1,3 +1,22 @@
 ﻿$(document).ready(function () {
-    $('#usuarios').DataTable();
+    $('#usuarios').DataTable({
+        ajax: {
+            url: 'https://localhost:7126/api/usuarios/buscarUsuarios'
+        },
+        columns: [
+            { data: 'id', title: 'Id' },
+            { data: 'nombre', title: 'Nombre' },
+            { data: 'apellido', title: 'Apellido' },
+            { data: 'fecha_Nacimiento', title: 'Fecha de Nacimiento' },
+            { data: 'mail', title: 'Mail' },
+            { data: 'id_Rol', title: 'Rol' },
+            { data: 'active', title: 'Activo' },
+            { data: 'codigo', title: 'Código' },
+        ],
+        language:
+        {
+            url: 'https://cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json'
+        }
+    });
 });
+
