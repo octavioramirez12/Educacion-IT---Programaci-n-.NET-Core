b4ec8a2ea43f351af7b19a4e1686b402671f5c63
Octavio Ramírez <octavio.ramirez.3980@gmail.com>
2023-12-18 08:00:34 -0300

    Desarrollo de método POST en baseApi Desarrollo de método GuardarUsuario en controller de usuarios de la web Registración de httpClient en program de la web

--- Data/Base/BaseApi.cs
+++ Data/Base/BaseApi.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data.Base
 {
-    internal class BaseApi
+    public class BaseApi : ControllerBase
     {
+        private readonly IHttpClientFactory _httpClient;
+
+        public BaseApi(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> PostToApi(string controllerMethodUrl, object model)
+        {
+            try
+            {
+                var client = _httpClient.CreateClient("useApi");
+
+                var response = await client.PostAsJsonAsync(controllerMethodUrl, model);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return Ok(content);
+                }
+                return BadRequest(response);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
--- Educacion IT - Programación .NET/Controllers/UsuariosController.cs
+++ Educacion IT - Programación .NET/Controllers/UsuariosController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Data.Base;
+using Data.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Educacion_IT___Programación_.NET.Controllers
 {
     public class UsuariosController : Controller
     {
+
+        private readonly IHttpClientFactory _httpClient;
+        public UsuariosController(IHttpClientFactory httpClient)
+        {
+            _httpClient= httpClient;
+        }
         public IActionResult Usuarios()
         {
             return View();
@@ -13,5 +21,12 @@ namespace Educacion_IT___Programación_.NET.Controllers
             return PartialView("~/Views/Usuarios/Partial/UsuariosAddPartial.cshtml");
         }
 
+        public async Task<IActionResult> GuardarUsuario(Usuarios usuario)
+        {
+            var baseApi = new BaseApi(_httpClient);
+            var usuarios = await baseApi.PostToApi("Usuarios/GuardarUsuario", usuario);
+
+            return RedirectToAction("Usuarios", "Usuarios");
+        }
     }
 }
